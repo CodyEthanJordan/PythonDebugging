935c2b646966e67fc3a75bd9cd6f8cb4e137687c
Cody Jordan <codyethanjordan@knights.ucf.edu>
2017-02-24 17:37:13 -0500

    Made mock function for garden

--- GitExample/hw7_cjordan.py
+++ GitExample/hw7_cjordan.py
@@ -3,3 +3,15 @@
 
 # Problem 1
 print('Problem 1')
+
+
+def garden(length, carrot=0, lettuce=0, pepper=0, tomato=0):
+    '''garden takes the length of my garden in feet and the desired yeild in pounds
+    returns a tuple indicating
+    -is this garden posible?
+    -total required length
+    -number of each plant required'''
+    
+    return (True, 10, 1, 2, 3, 4) #Mock output
+    
+print(garden(12, carrot=40, lettuce=2, pepper=6, tomato=3))
