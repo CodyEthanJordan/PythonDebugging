a4c926e475cb59fab5ac9b4d10c97441a603f4c7
CodyEthanJordan <codyethanjordan@gmail.com>
2017-02-27 12:52:32 -0500

    Finishing debug examples

--- DebuggingExamples/debugging.py
+++ DebuggingExamples/debugging.py
@@ -2,4 +2,42 @@
 # Introduction to Numerical Computing
 # Debugging
 
+# PDB documentation
+# https://docs.python.org/3/library/pdb.html
 
+import numpy as np
+
+name = "foo"
+x = np.linspace(0,2*np.pi)
+
+# double click next to line numbe to put breakpoint here
+# instead of hitting run hit Debug (Ctrl + F5)
+y = np.sin(x)
+
+# in pdb we can print values with "p", such as "p x"
+# why won't p y work?
+# Put in w to see where we are in code
+
+def fibonacci(n):
+    if(n == 0):
+        return 0
+    else:
+        return n + fibonacci(n)
+        
+fib = fibonacci(5)
+print(fib)
+
+# use "s" to enter function
+
+def squareTotal(a):
+    '''Calculates the sum of the squares of an array'''
+    for i in len(a):
+        a[i] = a[i]**2
+
+    total = 0
+    for i in len(a):
+        total = total + a[i]
+
+    return total
+    
+print(squareTotal(x))
--- DebuggingExamples/exceptionHandling.py
+++ DebuggingExamples/exceptionHandling.py
@@ -9,3 +9,15 @@
 
 x = int(input("Please enter a number: "))
 
+# if we don't put in a number we get an error, read the stack trace
+
+# Usually if we have an error we want to fix it, but sometimes we need to make
+# a program which deals with things outside our control
+
+try:
+    x = int(input("Please enter a number: "))    
+except ValueError:
+    print('Thats not a number!')
+    x = None
+    
+
